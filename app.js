1334d8d396cf56beaafc462bc7ec672c91797a3d
Matias Sciutto <sciuttomatias@gmail.com>
2020-10-22 21:28:22 -0300

    Creando Front de Formulario de contenido

--- app.js
+++ app.js
@@ -6,8 +6,16 @@ const port = 1000
 
 app.listen(port)
 
+app.use( express.static("public") )
+
 app.get(("/contacto"), (req, res) => {
 
     res.end("Desde aca vamos a contactarnos..")
+
+})
+
+app.post(("/enviar"), (req, res) => {
+
+    res.end("Desde aca vamos a hacer algo muy loco..")
 
 })
